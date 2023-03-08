e9ae1a009e349bce21023e16a94af0cbe1cc8c29
Ubaid-arif <giftprintingservice@gmail.com>
2023-03-09 00:23:07 +0500

    initialize

---
+++ src/Screen/MainScreen/WelcomeScreen/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native/types'
+
+const WelcomeScreen = () => {
+  return (
+    <View>
+        <Text>
+            Welcome Project
+        </Text>
+    </View>
+  )
+}
+
+export default WelcomeScreen
